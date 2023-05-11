9a505cbd2583b9d57eb5d02042b64f19e3ab9173
FVazquezRisso <facundovazquez321@gmail.com>
2023-05-11 09:07:04 -0300

    Homework clase 4 completo

--- FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1-master/04-EstructuraDeDatos-I/homework/homework.js
@@ -14,9 +14,25 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
-function nFactorial(n) {}
+function nFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else if (n < 0) {
+    return 0;
+  }
 
-function nFibonacci(n) {}
+  return n * nFactorial(n - 1);
+}
+
+function nFibonacci(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else if (n > 1) {
+    return nFibonacci(n - 1) + nFibonacci(n - 2);
+  }
+}
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -27,7 +43,29 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
 Pueden utilizar class o función constructora.
 */
 
-function Queue() {}
+class Queue {
+  constructor() {
+    this.array = [];
+  }
+
+  enqueue(e) {
+    this.array.push(e);
+  }
+
+  dequeue() {
+    if (this.array.length == 0) {
+      return undefined;
+    } else {
+      return this.array.shift();
+    }
+  }
+
+  size() {
+    return this.array.length;
+  }
+}
+
+
 
 /*⚠️ No modificar nada debajo de esta línea ⚠️*/
 module.exports = {
