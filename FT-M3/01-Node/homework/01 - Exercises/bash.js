933318639e028b10ec317fd06dd037371e80d50a
FVazquezRisso <facundovazquez321@gmail.com>
2023-06-19 16:19:35 -0300

    Cargando archivos M3

---
+++ FT-M3/01-Node/homework/01 - Exercises/bash.js
@@ -0,0 +1,28 @@
+const process = require('process');
+const { Z_ASCII } = require('zlib');
+const commands = require('./commands/index.js');
+
+function bash() {
+  process.stdout.write("prompt > ");
+  process.stdin.on("data", (data) => {
+    const args = String(data).trim().split(" ");
+    const cmd = args.shift();
+
+    if (cmd in commands) {
+      commands[cmd](print, args.join(" "));
+    } else {
+      print(`command not found: ${cmd}`);
+    }
+  });
+}
+
+function print(output) {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+}
+
+bash();
+module.exports = {
+   print,
+   bash,
+};
